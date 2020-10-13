28f09ad36ba83453fa9e628def944232b7e6cbbf
Cam Spencer <camspencer@hotmaill.com>
2020-10-12 21:55:26 -0400

    Removing messaging service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MessagesComponent } from './messages/messages.component';
 import { ShopDetailsComponent } from './shop-details/shop-details.component';
 import { ShopsComponent } from './shops/shops.component';
 
@@ -14,7 +13,6 @@ import { ShopsComponent } from './shops/shops.component';
     AppComponent,
     ShopsComponent,
     ShopDetailsComponent,
-    MessagesComponent,
     DashboardComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
--- src/data/shops-initializer.ts
+++ src/data/shops-initializer.ts
@@ -1,9 +1,9 @@
 import { Shop } from 'src/models/shop';
 
 export const SHOPS: Shop[] = [
-  { id: 11, name: 'The Arcanium' },
-  { id: 12, name: 'The Sword & Board' },
-  { id: 13, name: 'Brews Before Hags' },
-  { id: 14, name: 'Tiff & Sons' },
-  { id: 15, name: "Meg's Scrolls" },
+  { id: 1, name: 'The Arcanium' },
+  { id: 2, name: 'The Sword & Board' },
+  { id: 3, name: 'Brews Before Hags' },
+  { id: 4, name: 'Tiff & Sons' },
+  { id: 5, name: "Meg's Scrolls" },
 ];
